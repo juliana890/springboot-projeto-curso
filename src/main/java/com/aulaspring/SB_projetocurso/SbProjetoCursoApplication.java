87cc9f81d7a3c71b99a2b36edc2d115eb3c66e11
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-03-04 10:43:19 -0300

    Projeto criado

---
+++ src/main/java/com/aulaspring/SB_projetocurso/SbProjetoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.aulaspring.SB_projetocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbProjetoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbProjetoCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulaspring/SB_projetocurso/SbProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspring.SB_projetocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
