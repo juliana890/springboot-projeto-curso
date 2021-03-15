9f9248d7d20c684b792ffb580ab66e6ef16a2692
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-03-15 11:37:49 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/aulaspring/SB_projetocurso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/aulaspring/SB_projetocurso/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.aulaspring.SB_projetocurso.services.exceptions.DataIntegrityException;
 import com.aulaspring.SB_projetocurso.services.exceptions.ObjectNotFoundException;
 
 //Criamos essa classe para tratar possíveis erros no controlador Rest
@@ -16,6 +17,15 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class) //Tratador de exceções do tipo ObjectNotFoundException
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.NOT_FOUND; //Error 404
+		
+		StandardError err = new StandardError(status.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		
 		StandardError err = new StandardError(status.value(), e.getMessage(), System.currentTimeMillis());
 		
