901d339bf80492fc7a5669ac5a9ab3b4c7a74ea1
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-03-23 15:21:56 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/aulaspring/SB_projetocurso/domain/Cliente.java
+++ src/main/java/com/aulaspring/SB_projetocurso/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -30,7 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL) //Acrescentamos a o Cacade para quando um cliente for deletado do BD seus endereços sejam deletados tbm
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	//Para não conter repetições de números criamos uma coleção Set
